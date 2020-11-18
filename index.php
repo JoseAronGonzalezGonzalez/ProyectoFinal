829563d622a639026ea7449792cddbdf169ca571
JoseAronGonzalezGonzalez <josearong_18@alu.uabcs.mx>
2020-11-18 00:54:31 -0700

    prototipo o idea primera aproximadamente

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+	<link rel="stylesheet" type="text/css" href="./assets/css/estilo.css?v=0.0.3" media="all">
+	<title>Document</title>
+</head>
+<body>
+	<div class="barra-menu">
+		<div>
+			<img src="./assets/img/logoPelicula2.png">	
+		</div>
+		<div>
+			<ul>
+				<li>
+					
+				</li>
+			</ul>
+		</div>
+		<div class="btn-reg-log">
+			<!-- <button>iniciar secion</button> -->
+			<button id="btn-abrir-popup" class="btn-abrir-popup">Iniciar Sesion</button>
+			<button>registrar</button>
+		</div>
+	</div>
+
+	<div>
+		<h1>UNA IDEA PERO CREO QUE ASI NO LA VOY HACER LA PAGUINA</h1>
+	</div>
+
+
+	<div class="overlay" id="overlay">
+			<div class="popup" id="popup">
+				<a href="#" id="btn-cerrar-popup" class="btn-cerrar-popup"><i class="fas fa-times"></i></a>
+				<h3>inicio de sesion</h3>
+				<h4><!-- y recibe un cupon de descuento. --></h4>
+				<form action="">
+					<div class="contenedor-inputs">
+						
+						<input type="email" placeholder="Correo">
+						<input type="password" placeholder="Contraseña">
+					</div>
+					<input type="submit" class="btn-submit" value="Inicio">
+				</form>
+			</div>
+		</div>
+
+	<script src="./assets/js/js.js"></script>
+</body>
+</html>
