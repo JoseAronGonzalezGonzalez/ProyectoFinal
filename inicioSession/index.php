92dd7dec8adfb27cf814d31b855d7b0a20869228
JoseAronGonzalezGonzalez <josearong_18@alu.uabcs.mx>
2020-11-25 01:32:02 -0700

    inclucion login

---
+++ inicioSession/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../assets/css/estiloLogin.css?v=0.0.3" media="all">
+	<title>Loggin o registro</title>
+</head>
+<body>
+	<div class="login"> 
+		<img src="../assets/img/logoPelicula2.png">
+		<div class="loginHeder">
+			<div class="PrinciLogin">
+				<h1>Inicia sesion</h1>
+				<div>
+					<label>Correo:</label><br>
+					<input type="email" name="email1" id="email1" placeholder="ingresa tu correo"><br>
+					<label>Contraseña:</label><br>
+					<input type="password" name="email" id="email"><br>
+					<div class="areaRegistro">
+						<button>Iniciar sesion</button><br>
+
+						<label>¿Quires registrarte?</label><a href="">Resgistrate</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div>
+			<p>
+ 				<span>Privacy Policy</span> | This is a sample website - cmsmasters © 2020 / All Rights Reserved
+ 			</p>
+		</div>
+	</div>
+
+</body>
+</html>
