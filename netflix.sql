537ab98d6112ab9ef69c287dfebdc92297d898b5
JoseAronGonzalezGonzalez <josearong_18@alu.uabcs.mx>
2020-12-03 12:57:14 -0700

    se agrego columna de roles a la tabla de usuarios

--- netflix.sql
+++ netflix.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-12-2020 a las 14:23:23
+-- Tiempo de generación: 03-12-2020 a las 20:55:39
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.2.31
 
@@ -40,8 +40,9 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`, `description`, `status`, `created_at`) VALUES
-(1, 'De terror', 'peliculas de miedo', 'active', '2020-11-19 20:34:35'),
-(3, 'dramas', 'películas que drama', 'active', '2020-11-19 20:34:39');
+(1, 'De terror', 'peliculas miedo', 'active', '2020-11-19 20:34:35'),
+(3, 'dramas', 'películas que drama', 'active', '2020-11-19 20:34:39'),
+(12, 'comedia', 'el mejor', 'active', '2020-12-03 00:44:39');
 
 -- --------------------------------------------------------
 
@@ -80,17 +81,23 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL DEFAULT 'active',
-  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `rol` varchar(45) NOT NULL DEFAULT 'cliente'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `status`, `created_at`) VALUES
-(3, 'jose', 'j@h', 'f1475568c0c5e359eac1eddc6e5e70eefbfc7cd2', 'active', '2020-11-26 21:50:42'),
-(4, 'lolcito', 'jose_@hm', 'f1475568c0c5e359eac1eddc6e5e70eefbfc7cd2', 'active', '2020-11-26 22:02:47'),
-(9, '123122312', 'j@h1', 'f1475568c0c5e359eac1eddc6e5e70eefbfc7cd2', 'active', '2020-12-01 05:24:32');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `status`, `created_at`, `rol`) VALUES
+(-1, '18+', 'elmejor@alu', 'f1475568c0c5e359eac1eddc6e5e70eefbfc7cd2', 'active', '2020-12-02 23:39:37', 'cliente'),
+(3, 'jose', 'j@h', 'f1475568c0c5e359eac1eddc6e5e70eefbfc7cd2', 'active', '2020-11-26 21:50:42', 'cliente'),
+(4, 'lolcito', 'jose_@hm', 'f1475568c0c5e359eac1eddc6e5e70eefbfc7cd2', 'active', '2020-11-26 22:02:47', 'cliente'),
+(9, '123122312', 'j@h1', 'f1475568c0c5e359eac1eddc6e5e70eefbfc7cd2', 'active', '2020-12-01 05:24:32', 'cliente'),
+(10, 'el mejor', 'jose@aron', 'f1475568c0c5e359eac1eddc6e5e70eefbfc7cd2', 'active', '2020-12-01 19:49:35', 'cliente'),
+(50, 'Jose Aron Gonzalez', 'admi@admi', 'f1475568c0c5e359eac1eddc6e5e70eefbfc7cd2', 'active', '2020-12-03 00:32:18', 'administrador'),
+(51, 'lol123', 'elkaka@k', 'f1475568c0c5e359eac1eddc6e5e70eefbfc7cd2', 'active', '2020-12-03 00:36:40', 'cliente'),
+(52, 'admi2', 'admi2@admi2', 'f1475568c0c5e359eac1eddc6e5e70eefbfc7cd2', 'active', '2020-12-03 00:46:23', 'administrador');
 
 --
 -- Índices para tablas volcadas
@@ -112,7 +119,8 @@ ALTER TABLE `movies`
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -122,7 +130,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `movies`
@@ -134,7 +142,7 @@ ALTER TABLE `movies`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
